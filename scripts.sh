9991c3d31804720f120c2e71b53805d934241974
Mohansai <bandarumohansai@gmail.com>
2024-11-14 13:13:43 +0530

    few changes

--- scripts.sh
+++ scripts.sh
@@ -21,13 +21,16 @@ xfs_growfs /
 xfs_growfs /var
 df -hT
 
+#Uninstalling the older docker version if exists:
+sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 
 #2) installing the docker:
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo -y
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
-usermod -aG docker ec2-user
+sudo usermod -aG docker ec2-user
+# echo "Please relogin to this server/workstation to reflect the group changes"
 
 
 #3) install the kubectl command line tool
@@ -48,7 +51,29 @@ tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 mv /tmp/eksctl /usr/local/bin
 
 
-# 5) aws configure  ----> this is done in provider .tf
+# 5) aws configure:
+# 5-i) installing aws cli:
+
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+
+# # 5-2)passing the aws creds from terraform variables(run time) to shell here:
+# AWS_ACCESS_KEY_ID=$1
+# AWS_SECRET_ACCESS_KEY=$2
+# AWS_REGION=$3
+
+# echo "aws_access key is $AWS_ACCESS_KEY_ID"
+# echo "aws_secret access key is $AWS_SECRET_ACCESS_KEY"
+# echo "aws_region is $AWS_REGION"
+
+# # 5-3) Configuring the aws:
+
+# aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+# aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+# aws configure set aws_region "$AWS_REGION"
+# aws configure set output json
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -3,3 +3,14 @@
     
 # }
 
+# variable "AWS_ACCESS_KEY_ID" {
+
+# }
+
+# variable "AWS_SECRET_ACCESS_KEY" {
+
+# }
+
+# variable "AWS_REGION" {
+
+# }
